7b0c915de9fe496ecd5edd3468bed1915889e6d4
briandgomez <briandgomez98@gmail.com>
2021-12-17 23:03:24 -0500

    user routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,6 +5,56 @@ const userController = {
         User.create(body)
             .then(dbUserData => res.json(dbUserData))
             .catch(err => res.json(err));
+    },
+    getAllUsers(req, res) {
+        User.find({})
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData));
+    },
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(400).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err))
+    },
+    deleteUser({ params }, res) {
+        User.findByIdAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(400).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err))
+    },
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(400).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
     }
 }
 
--- routes/api/user-route.js
+++ routes/api/user-route.js
@@ -1,12 +1,20 @@
 const router = require('express').Router();
 
 const {
-    createUser
+    createUser, getAllUsers, updateUser, deleteUser, getUserById
 } = require('../../controllers/user-controller');
 
 // api/users
 router
 .route('/')
 .post(createUser)
+.get(getAllUsers);
+
+// api/users/:id
+router
+.route('/:id')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser);
 
 module.exports = router;
